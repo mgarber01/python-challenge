4d130ad6088016a3f610b2181349fc62caf79bfc
mgarber01 <mattyg.golf@gmail.com>
2018-12-05 17:17:35 -0600

    commented out and complete

--- pybank/main.py
+++ pybank/main.py
@@ -8,15 +8,17 @@ with open(csvfile, newline= '') as csvfile:
     csv_header = next(csvreader)
     
 
-    totalp = []
-    change = []
-    profit = 0
-    p = []
-    changelist = []
-    totalc = 0
+    totalp = [] # total profit refers to the list of the month and profit
+    change = [] # change from one month to the next used to define the list that is appened to changelist
+    profit = 0 # refers to the raw profit number
+    p = [] # used to define the list that is appended to totalp
+    changelist = [] # list of change and monht
+    totalc = 0 # raw number total sum of 
     data = [row for row in csvreader]
-    GD = ["",0]
-    GI = ["",0]
+    GD = ["",0] # Greatest Decrease shell
+    GI = ["",0] # Greatest Increase shell 
+
+    # looping thru the data to grab total p, profit and change in the if else statement looking one row ahead 
     for i in range(0,len(data)): 
           p =[data[i][0], int(data[i][1])]
           totalp.append(p)
@@ -28,7 +30,7 @@ with open(csvfile, newline= '') as csvfile:
               changelist.append(change)
               totalc += change[1]  
             
-                
+    # now that Change has been established we loop thru the Changelist to grab GD and DI            
     for j in range(0,len(changelist)):
         if (j+1)>= len(changelist):
             pass
@@ -37,7 +39,7 @@ with open(csvfile, newline= '') as csvfile:
         elif changelist[j][1]< 0 and changelist[j][1] < changelist[j+1][1] and changelist[j][1] < GD[1]:
             GD = changelist[j]            
     
-
+#Print to the terminal
 print('Financial Analysis\n-----------------------')
 print(f'Total Months:{len(data)}')
 print(f'Total: {profit}')
@@ -45,6 +47,7 @@ print(f'Average Change: {round((totalc/(len(data)-1)),2)}')
 print(f'Greatest Increase in Profits: {GI[0]} ({GI[1]})')
 print(f'Greatest Decrease in Profits: {GD[0]} ({GD[1]})') 
 
+#print to the .txt file. 
 bankresultsfile = open("C:\\Users\\Matt Garber\\python-challenge\\bankresults.txt","w")
 bankresultsfile.write("\n Financial Analysis \n ----------------\n Total Months:86\nTotal: 38382578\n Average Change: -2315.12\n Greatest Increase in Profits: Feb-2012 (1926159)\n Greatest Decrease in Profits: Sep-2013 (-2196167)")
 bankresultsfile.close()
--- pypoll/main.py
+++ pypoll/main.py
@@ -5,21 +5,21 @@ with open(csvfile, newline= '') as csvfile:
     csvreader = csv.reader(csvfile,delimiter= ',')
     print(csvreader)
     csv_header = next(csvreader)
-    cd = ""
-    count = 1
-    tcount = 0
-    lcount = 1
-    vtcountlist = []
-    data = [row[2] for row in csvreader]
+    cd = "" # roughly speaking this is a variable to denote the candidate
+    count = 1 # count was used and reset based on the count of votes/candidate
     
-    cdlist = []
+    vtcountlist = [] # vote count list .. the candidate themselves and  the number of times the canidate appeared in the list was the vote count
+    data = [row[2] for row in csvreader] # since the other two data elements were not important did list completion on just candidate
+    
+    cdlist = [] 
     done = len(data)
     for i in range(0,len(data)):
-        if data[i] not in cdlist:
+        if data[i] not in cdlist: # my favorite part of the program .. I forgot about the in operator stuck for a long time figuring how to get the list of unique candidates
             
             cd = data[i] 
         
             cdlist.append(cd)
+            # now that we have a list of unique candidates we loop thru and count how many times each one appears 
     for j in range(0,len(cdlist)):
         for i in range(0,len(data)):
             if cdlist[j] == data[i]:
@@ -38,7 +38,7 @@ with open(csvfile, newline= '') as csvfile:
         
         
         
-        
+    # Printing to the terminal    
     print('Election Results\n ----------------------')   
     print(f'Total Votes: {len(data)}\n --------------------')     
     print(f'{vtcountlist[0][0]}: {round(((vtcountlist[0][1]/len(data))* 100),2)}% ({vtcountlist[0][1]})')
@@ -46,6 +46,7 @@ with open(csvfile, newline= '') as csvfile:
     print(f'{vtcountlist[2][0]}: {round(((vtcountlist[2][1]/len(data))* 100),2)}% ({vtcountlist[2][1]})')
     print(f'{vtcountlist[3][0]}: {round(((vtcountlist[3][1]/len(data))* 100),2)}% ({vtcountlist[3][1]})')  
 
+# printing to the .txt file
 resultsfile = open("C:\\Users\\Matt Garber\\python-challenge\\pollresults.txt","w")
 resultsfile.write(("\n Election Results\n---------------------\n Total Votes: 3521001\n--------------------\nKhan: 63.0% (2218232)\nCorrey: 20.0% (704201)\nLi: 14.0% (492941)\nO'Tooley: 3.0% (105631)"))    
 resultsfile.close()          
